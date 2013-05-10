1f63ac861fe7f8ddddba699327d7bf628f11e86d
Corey Purdy <coreypizzle@gmail.com>
2013-05-10 14:47:55 +0100

    initial

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+
+  validates_presence_of :username
+  validates_uniqueness_of :username
+  attr_accessor :login
+  attr_accessible :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
+  # attr_accessible :title, :body
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openideas::Application.config.secret_token = 'aee0d2f60a01c7b9e45d46d65e4b497ebd15672776c6541a3d78f7a428e8ac35abe7f35c607a76941a0bb0667ad2a1378c0d32d6c740512c48ecd2b089258a48'
